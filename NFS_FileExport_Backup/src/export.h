5a6c4ad438deddf5c65d8aedb1f23fa1fda7472e
Chethan.Y <chethan.casey@gmail.com>
2013-11-19 14:34:01 -0500

    Updating LPGL2.1 License to export.h

--- NFS_FileExport_Backup/src/export.h
+++ NFS_FileExport_Backup/src/export.h
@@ -1,3 +1,25 @@
+/* Copyright (C) 2013-14 Green Turtles Technologies <www.greenturtles.in>, Inc.  All rights reserved.
+
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Authors: Sunil Kumar <skjmca51@gmail.com>
+            Ganesh Kumar <ganeshkumar14@gmail.com>
+ * -*- coding: utf-8 -*-
+ */
+
+
 #ifndef EXPORT_H
 #define EXPORT_H
 
